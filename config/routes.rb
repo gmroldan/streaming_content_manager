1bfee05ff4c849e311375496871df1fb7b2592de
Gerardo Martín Roldán <gmroldanutn@gmail.com>
2019-08-27 22:56:55 -0300

    New users screen

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  #get 'users/index'
   devise_for :users
   get 'home/index'
   root to: "videos#index"
   resources :videos
+  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
